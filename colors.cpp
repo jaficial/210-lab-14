38a60a148d3ceff1754bca1838a61d5968581123
Jed Aficial's PC <jedaficial1@gmail.com>
2024-09-28 22:37:39 -0700

    Adding comments to the code

--- colors.cpp
+++ colors.cpp
@@ -6,24 +6,23 @@
 
 using namespace std;
 
-/*NOTE: SIMPLE CLASS FUNCTION: color class should have three private member variables
- * all members should be ints
- * the three ints should represent the three rgb values 
- * mustuse setter/getter member functions
- * the values must only be 0-255
- * also create a 'print' function to output the values to the console*/
-
+/* set max values for 0-255 for rgb values incase I'd like
+   to edit this program into taking input from console/file*/
 const int max_rgb = 255;
 const int min_rgb = 0;
 
+/* 'color' class contains 4 private member variables, three
+	of the have type int for taking in a RGB value from 0-255.
+	The last member is for taking in a string of the color name. */ 
 class color {
 private:
 	int red_value;
 	int green_value;
 	int blue_value;
 	string color_name;
+
 public:
-	int getred_value() {return red_value;} // 
+	int getred_value() {return red_value;} // getter returns 
 	void setred_value(int red) {red_value = red;}
 	
 	int getgreen_value() {return green_value;}
@@ -36,10 +35,12 @@ public:
 	void setcolor_name(string name) {color_name = name;}
 
 	void print() { // NOTE: COME BACK TO THIS FUNCTION TO FIX OUTPUT OF VALUES
-		cout << "RGB Values for " << color_name << endl;
-		cout << setw(15) << "RED VALUE: " << red_value << endl;
-		cout << setw(15) << "GREEN VALUE: " << green_value << endl;
-		cout << setw(15) << "BLUE VALUE: " << blue_value << endl;
+		cout << endl << "RGB Values for " << color_name << ":" << endl;
+		cout << "---------------------------" << endl;
+		cout << setw(18) << "RED VALUE: " << red_value << endl;
+		cout << setw(18) << "GREEN VALUE: " << green_value << endl;
+		cout << setw(18) << "BLUE VALUE: " << blue_value << endl;
+		cout << "---------------------------" << endl;
 	}
 };
 
