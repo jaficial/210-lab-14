a99be3f105f45ec9efb9c6e12e60162b422d044d
Jed Aficial's PC <jedaficial1@gmail.com>
2024-09-28 22:46:39 -0700

    Finished Lab 14

--- colors.cpp
+++ colors.cpp
@@ -12,7 +12,7 @@ const int max_rgb = 255;
 const int min_rgb = 0;
 
 /* 'color' class contains 4 private member variables, three
-	of the have type int for taking in a RGB value from 0-255.
+	of them have type int for taking in a RGB value from 0-255.
 	The last member is for taking in a string of the color name. */ 
 class color {
 private:
@@ -22,10 +22,14 @@ private:
 	string color_name;
 
 public:
-	int getred_value() {return red_value;} // getter returns 
-	void setred_value(int red) {red_value = red;}
+	int getred_value() {return red_value;} // getter returns the red RGB value 
+	void setred_value(int red) {red_value = red;} // setter assigns the received red RGB value to red_value
 	
-	int getgreen_value() {return green_value;}
+	/* getter and setter for 'green_value', 'blue_value', 'getcolor_name'
+	   work exactly the same as the getter and setter for 'red_value'.
+	   Only difference is that 'getcolor_name' gets and sets a string vs. 
+	   getting and setting an int for the RGB values variables. */
+	int getgreen_value() {return green_value;} 
 	void setgreen_value(int green) {green_value = green;}
 
 	int getblue_value() {return blue_value;}
@@ -34,7 +38,8 @@ public:
 	string getcolor_name() {return color_name;}
 	void setcolor_name(string name) {color_name = name;}
 
-	void print() { // NOTE: COME BACK TO THIS FUNCTION TO FIX OUTPUT OF VALUES
+	// 'print' function outputs the name of the color, and its RGB values
+	void print() {
 		cout << endl << "RGB Values for " << color_name << ":" << endl;
 		cout << "---------------------------" << endl;
 		cout << setw(18) << "RED VALUE: " << red_value << endl;
@@ -46,7 +51,9 @@ public:
 
 void print(color);
 
-// NOTE: FUNCTION IS NOW PRINTING
+/* CITED: Cited the class lecture code when testing with the setw() function
+   as well as for testing and learning the syntax of writing a basic class */
+// 3 color objects are written and defined in the main function
 int main() {
 
 	color brown;
@@ -54,7 +61,6 @@ int main() {
 	brown.setred_value(150);
 	brown.setgreen_value(75);
 	brown.setblue_value(0);
-	
 	brown.print();
 
 	color wine;
